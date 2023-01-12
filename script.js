32e6b9a751d38eb8f792828fa122da7bba581a2e
Andrew Roide <andrew.roide@hotmail.com>
2023-01-11 17:59:47 -0800

    added book constructor

---
+++ script.js
@@ -0,0 +1,10 @@
+let library = [];
+
+class book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
