1bf039f0200f0301d2ee679ec6c8d3edc7bc9ad3
Andrew Roide <andrew.roide@hotmail.com>
2023-01-29 11:20:42 -0800

    switching from object construct to factory

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-let library = [];
 
-class book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-}
+(function() {
+    
+})()
