4a656b3f51f9dea1e4ddafe0fdb37d4a1517627c
Andrew Roide <andrew.roide@hotmail.com>
2023-02-09 16:10:54 -0800

    getting book data, but array is empty

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 (function() {
+
     let myLibrary = [];
+
+    class AddedBook {
+        constructor(title, author, pages, read) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.read = read;
+        }
+    }
+
+    function myFormData() {
+        const title = document.getElementById('bookTitle').value;
+        const author = document.getElementById('bookAuthor').value;
+        const pages = document.getElementById('bookPages').value;
+        const read = document.getElementById('read').value;
+
+        const newBook = new AddedBook(title, author, pages, read);
+        myLibrary.push(newBook);
+    }
+    myFormData();
+    console.log(myLibrary);
+
+
 })()
