d512ac0a338e9d7b37f39079fdcf7a2b440dd934
Andrew Roide <andrew.roide@hotmail.com>
2023-02-13 21:45:17 -0800

    form values now pushing to empty array. Added reset to clear form

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-(function() {
+let myLibrary = [];
 
-    let myLibrary = [];
+// Constructor Example
+class AddedBook {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
 
-    class AddedBook {
-        constructor(title, author, pages, read) {
-            this.title = title;
-            this.author = author;
-            this.pages = pages;
-            this.read = read;
-        }
-    }
+// ----Factory Function example----
+// const AddedBook = (title, author, pages, read) => {
+//   return {title, author, pages, read};
+// }
 
-    function myFormData() {
-        const title = document.getElementById('bookTitle').value;
-        const author = document.getElementById('bookAuthor').value;
-        const pages = document.getElementById('bookPages').value;
-        const read = document.getElementById('read').value;
+function myFormData() {
+  const title = document.getElementById("bookTitle").value;
+  const author = document.getElementById("bookAuthor").value;
+  const pages = document.getElementById("bookPages").value;
+  const read = document.querySelector('[name="ifRead"]').value;
+  
+  const newBook = new AddedBook(title, author, pages, read);
+  
+  // ----Factory Function example calling function----
+  // const newBook = AddedBook(title, author, pages, read)
 
-        const newBook = new AddedBook(title, author, pages, read);
-        myLibrary.push(newBook);
-    }
-    myFormData();
-    console.log(myLibrary);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+}
 
-
-})()
+function clearInput() {
+  document.getElementById('dataForm').reset();
+}
