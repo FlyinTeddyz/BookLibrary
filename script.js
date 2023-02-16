392e47db29d6bd31f4c8bac3d0c054fbda28f74f
Andrew Roide <andrew.roide@hotmail.com>
2023-02-15 18:03:25 -0800

    formating and added clearinput function for form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// ----Constructor Example----
+
 let myLibrary = [];
 
-// Constructor Example
 class AddedBook {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -10,26 +11,40 @@ class AddedBook {
   }
 }
 
-// ----Factory Function example----
-// const AddedBook = (title, author, pages, read) => {
-//   return {title, author, pages, read};
-// }
-
 function myFormData() {
   const title = document.getElementById("bookTitle").value;
   const author = document.getElementById("bookAuthor").value;
   const pages = document.getElementById("bookPages").value;
   const read = document.querySelector('[name="ifRead"]').value;
-  
-  const newBook = new AddedBook(title, author, pages, read);
-  
-  // ----Factory Function example calling function----
-  // const newBook = AddedBook(title, author, pages, read)
+ 
+  const newBook = new AddedBook(title, author, pages, read)
 
   myLibrary.push(newBook);
   console.log(myLibrary);
 }
 
 function clearInput() {
-  document.getElementById('dataForm').reset();
+  document.getElementById('myForm').reset();
 }
+
+/*----Factory Function example----
+const AddedBook = (title, author, pages, read) => {
+  return {title, author, pages, read};
+}
+
+function myFormData() {
+const newBook = AddedBook(title, author, pages, read)
+myLibrary.push(newBook)
+}
+*/
+
+/*----Collecting From data refactored----
+function myFormData() {
+  var formElements = document.getElementById("myForm").elements;
+  var postData = {};
+  for (var i = 0; i < formElements.length; i++)
+    if (formElements[i].type != "submit")
+      postData[formElements[i].name] = formElements[i].value;
+  
+  myLibrary.push(postData)
+}*/
